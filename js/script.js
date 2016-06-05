5076220b963fd02ec832466c77dd67761dea9656
Flávio Wander de Araújo Batista <flaviowander@unifei.edu.br>
2016-06-05 20:26:16 -0300

    Facebook vw fix

--- js/script.js
+++ js/script.js
@@ -12,9 +12,12 @@ $(document).on('click','.navbar-collapse.in',function(e) {
 
 
 function navbarCheck(){
-    if($(window).width() <= 767){        
-        $("#mainNavbar").removeClass("mainNavbarFull");
-        $("#mainNavbar").removeClass("mainNavbarLogo");
+    if($(window).width() <= 991){        
+        if($(window).width() <= 767){        
+            $("#mainNavbar").removeClass("mainNavbarFull");
+            $("#mainNavbar").removeClass("mainNavbarLogo");
+        }
+        supportVhVw.setMargin(".aboutAda",4,3,4,3);
     }
     $('#mainBackground').css({'background-image': 'url(img/'+(Math.floor(Math.random()*10000)%20)+'.jpg)'});
     $("#btnmainBackground, #btnabout, #btncontact").on('click', function(e){
@@ -25,6 +28,16 @@ function navbarCheck(){
            scrollTop: target.offset().top
         }, 1000);
     });
+
+    // Init object
+    var supportVhVw = new SupportVhVw();
+
+    // Scale all texts
+    supportVhVw.setMargin(".aboutText",4,3,4,3);
+    supportVhVw.setMargin(".contactText",4,3,4,3);
+    supportVhVw.setMargin(".contactTextTitle",1,3,0,3);
+    supportVhVw.setPadding(".bodyText",5,0,0,0);
+    supportVhVw.setPadding("#logoImage",30,10,0,10);
 }
 
 $(window).load(navbarCheck);
@@ -43,3 +56,49 @@ $(window).scroll(function () {
     	$("#mainNavbar").addClass("mainNavbarLogo");
     }
 });
+
+function SupportVhVw() {
+    this.setPadding = function(name,top,right,bottom,left) {
+        jQuery(window).resize( function(event) {
+            scalePadding(name,top,right,bottom,left);
+        });
+        scalePadding(name,top,right,bottom,left);
+    }
+
+    this.setMargin = function(name,top,right,bottom,left) {
+        jQuery(window).resize( function(event) {
+            scaleMargin(name,top,right,bottom,left);
+        });
+        scaleMargin(name,top,right,bottom,left);
+    }
+    
+    var scaleMargin = function (name,top,right,bottom,left){
+        var scrWidth = jQuery(document).width();
+        var scrHeight = jQuery(document).height();
+
+        top = (scrHeight * top) / 100;
+        right = (scrWidth * right) / 100;        
+        bottom = (scrHeight * bottom) / 100;
+        left = (scrWidth * left) / 100;        
+
+        var marginTop = jQuery(name).css('margin-top', top + "px");
+        var marginRight = jQuery(name).css('margin-right', right + "px");        
+        var marginBottom = jQuery(name).css('margin-bottom', bottom + "px");
+        var marginLeft = jQuery(name).css('margin-left', left + "px");
+    }
+
+    var scalePadding = function (name,top,right,bottom,left){
+        var scrWidth = jQuery(document).width();
+        var scrHeight = jQuery(document).height();
+
+        top = (scrHeight * top) / 100;
+        right = (scrWidth * right) / 100;        
+        bottom = (scrHeight * bottom) / 100;
+        left = (scrWidth * left) / 100;
+
+        var paddingTop = jQuery(name).css('padding-top', top + "px");
+        var paddingRight = jQuery(name).css('padding-right', right + "px");        
+        var paddingBottom = jQuery(name).css('padding-bottom', bottom + "px");
+        var paddingLeft = jQuery(name).css('padding-left', left + "px");
+    }
+};
