52938fcc8dc584dbc75a44ffaaab6bf9e27d1dd9
Flávio Wander de Araújo Batista <flaviowander@unifei.edu.br>
2016-06-06 00:22:54 -0300

    hu

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ function setActive(d,index){
 	$(d).addClass("active");	
 }
 
+alert("");
+
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
         $(this).collapse('hide');
@@ -94,7 +96,7 @@ function SupportVhVw() {
         right = (scrWidth * right) / 100;        
         bottom = (scrHeight * bottom) / 100;
         left = (scrWidth * left) / 100;
-        
+
         var paddingTop = jQuery(name).css('padding-top', top + "px");
         var paddingRight = jQuery(name).css('padding-right', right + "px");        
         var paddingBottom = jQuery(name).css('padding-bottom', bottom + "px");
