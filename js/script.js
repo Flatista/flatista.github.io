16efa6bbd6d962d2ea1047113c75c491d618a19b
Flávio Wander de Araújo Batista <flaviowander@unifei.edu.br>
2016-06-07 01:35:54 -0300

    Facebook fix test

--- js/script.js
+++ js/script.js
@@ -3,25 +3,23 @@ function setActive(d,index){
 	$(d).addClass("active");	
 }
 
-alert("");
-
 $(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
         $(this).collapse('hide');
     }
 });
 
-
-
-function navbarCheck(){
+function loaded(){
     if($(window).width() <= 991){        
         if($(window).width() <= 767){        
             $("#mainNavbar").removeClass("mainNavbarFull");
             $("#mainNavbar").removeClass("mainNavbarLogo");
         }
-        supportVhVw.setMargin(".aboutAda",4,3,4,3);
+        setMargin(".aboutAda",4,3,4,3);
     }
+
     $('#mainBackground').css({'background-image': 'url(img/'+(Math.floor(Math.random()*10000)%20)+'.jpg)'});
+
     $("#btnmainBackground, #btnabout, #btncontact").on('click', function(e){
         e.preventDefault();
         var target = "#"+$(this).get(0).id.substring(3);
@@ -31,75 +29,125 @@ function navbarCheck(){
         }, 1000);
     });
 
-    // Init object
-    var supportVhVw = new SupportVhVw();
-
-    // Scale all texts
-    supportVhVw.setMargin(".aboutText",4,3,4,3);
-    supportVhVw.setMargin(".contactText",4,3,4,3);
-    supportVhVw.setMargin(".contactTextTitle",1,3,0,3);
-    supportVhVw.setPadding(".bodyText",5,0,0,0);
-    supportVhVw.setPadding("#logoImage",30,10,0,10);
+    setMargin(".aboutText",4,3,4,3);
+    setMargin(".contactText",4,3,4,3);
+    setMargin(".contactTextTitle",1,3,0,3);
+    setPadding(".bodyText",5,0,0,0);
+    setPadding("#logoImage",30,10,0,10);
+    setSize("#mainBackground",100,100);
 }
 
-$(window).load(navbarCheck);
+$(window).load(loaded);
 
-$(window).scroll(function () {
-    var e = $(this).scrollTop();
-    if (e > 120) {
-    	$("#iconLogo").removeClass("brandIconLogo");
-    	$("#iconLogo").addClass("brandIconFull");
-    	$("#mainNavbar").removeClass("mainNavbarLogo");
-    	$("#mainNavbar").addClass("mainNavbarFull");
-    } else {
-    	$("#iconLogo").removeClass("brandIconFull");
-    	$("#iconLogo").addClass("brandIconLogo");
-    	$("#mainNavbar").removeClass("mainNavbarFull");
-    	$("#mainNavbar").addClass("mainNavbarLogo");
-    }
-});
 
-function SupportVhVw() {
-    this.setPadding = function(name,top,right,bottom,left) {
-        jQuery(window).resize( function(event) {
-            scalePadding(name,top,right,bottom,left);
-        });
-        scalePadding(name,top,right,bottom,left);
-    }
-
-    this.setMargin = function(name,top,right,bottom,left) {
-        jQuery(window).resize( function(event) {
-            scaleMargin(name,top,right,bottom,left);
-        });
-        scaleMargin(name,top,right,bottom,left);
-    }
-    
-    var scaleMargin = function (name,top,right,bottom,left){
-        var scrWidth = $(window).width();
-        var scrHeight = $(window).height();
-
-        top = (scrHeight * top) / 100;
-        right = (scrWidth * right) / 100;        
-        bottom = (scrHeight * bottom) / 100;
-        left = (scrWidth * left) / 100;     
-        var marginTop = jQuery(name).css('margin-top', top + "px");
-        var marginRight = jQuery(name).css('margin-right', right + "px");        
-        var marginBottom = jQuery(name).css('margin-bottom', bottom + "px");
-        var marginLeft = jQuery(name).css('margin-left', left + "px");
-    }
-
-    var scalePadding = function (name,top,right,bottom,left){
-        var scrWidth = $(window).width();
-        var scrHeight = $(window).height();
+function setPadding (name,top,right,bottom,left) {
+    function scalePadding (name,top,right,bottom,left){
+        var scrWidth = window.innerWidth;
+        var scrHeight = window.innerHeight;
 
         top = (scrHeight * top) / 100;
         right = (scrWidth * right) / 100;        
         bottom = (scrHeight * bottom) / 100;
         left = (scrWidth * left) / 100;
 
-        var paddingTop = jQuery(name).css('padding-top', top + "px");
-        var paddingRight = jQuery(name).css('padding-right', right + "px");        
-        var paddingBottom = jQuery(name).css('padding-bottom', bottom + "px");
-        var paddingLeft = jQuery(name).css('padding-left', left + "px");
+        if (name.charAt(0) === '.') {
+            name = name.substr(1);
+            var cols = document.getElementsByClassName(name);
+            for(i=0; i<cols.length; i++) {
+                cols[i].style.paddingTop = top+"px";  
+                cols[i].style.paddingRight = right+"px";  
+                cols[i].style.paddingBottom = bottom+"px";  
+                cols[i].style.paddingLeft = left+"px";
+            }
+        }else if (name.charAt(0) === '#') {
+            name = name.substr(1);
+            document.getElementById(name).style.paddingTop = top+"px";  
+            document.getElementById(name).style.paddingRight = right+"px";  
+            document.getElementById(name).style.paddingBottom = bottom+"px";  
+            document.getElementById(name).style.paddingLeft = left+"px";
+        }
     }
-};
+
+    jQuery(window).resize( function(event) {
+        scalePadding(name,top,right,bottom,left);
+    });
+    scalePadding(name,top,right,bottom,left);
+}
+
+function setMargin(name,top,right,bottom,left) {
+    function scaleMargin (name,top,right,bottom,left){
+        var scrWidth = window.innerWidth;
+        var scrHeight = window.innerHeight;
+
+        top = (scrHeight * top) / 100;
+        right = (scrWidth * right) / 100;        
+        bottom = (scrHeight * bottom) / 100;
+        left = (scrWidth * left) / 100;
+
+        if (name.charAt(0) == '.') {
+            name = name.substr(1);
+            var cols = document.getElementsByClassName(name);
+            for(i=0; i< cols.length; i++) {
+                cols[i].style.marginTop = top+"px";  
+                cols[i].style.marginRight = right+"px";  
+                cols[i].style.marginBottom = bottom+"px";  
+                cols[i].style.marginLeft = left+"px";
+            }
+        }else if (name.charAt(0) == '#') {
+            name = name.substr(1);
+            document.getElementById(name).style.marginTop = top+"px";  
+            document.getElementById(name).style.marginRight = right+"px";  
+            document.getElementById(name).style.marginBottom = bottom+"px";  
+            document.getElementById(name).style.marginLeft = left+"px";
+        }
+    }                               
+
+    jQuery(window).resize( function(event) {
+        scaleMargin(name,top,right,bottom,left);
+    });
+    scaleMargin(name,top,right,bottom,left);
+}
+
+function setSize(name,height,width) {
+    function scaleSize (name,height,width){
+        var scrWidth = window.innerWidth;
+        var scrHeight = window.innerHeight;
+
+        height = (scrHeight * height) / 100;
+        width = (scrWidth * width) / 100;
+
+        if (name.charAt(0) == '.') {
+            name = name.substr(1);
+            var cols = document.getElementsByClassName(name);
+            for(i=0; i< cols.length; i++) {
+                cols[i].style.height = height+"px";
+                cols[i].style.width = width+"px"
+            }
+        }else if (name.charAt(0) == '#') {
+            name = name.substr(1);
+            document.getElementById(name).style.height = height+"px";
+            document.getElementById(name).style.width = width+"px"
+        }
+    }                               
+
+    jQuery(window).resize( function(event) {
+        scaleSize(name,height,width);
+    });
+    scaleSize(name,height,width);
+}
+
+
+document.addEventListener('scroll', function (event) {
+    var e =  (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;;
+    if (e > 200) {
+        $("#iconLogo").removeClass("brandIconLogo");
+        $("#iconLogo").addClass("brandIconFull");
+        $("#mainNavbar").removeClass("mainNavbarLogo");
+        $("#mainNavbar").addClass("mainNavbarFull");
+    } else {
+        $("#iconLogo").removeClass("brandIconFull");
+        $("#iconLogo").addClass("brandIconLogo");
+        $("#mainNavbar").removeClass("mainNavbarFull");
+        $("#mainNavbar").addClass("mainNavbarLogo");
+    }
+});
